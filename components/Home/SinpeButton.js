fcf7aaa9b742ddd280843af7e3ba7de6706473b9
Jose Armando Ramírez Rodríguez <jramirez@Joses-MacBook-Pro.local>
2024-11-11 01:52:10 -0600

    Home screen scalable folders

---
+++ components/Home/SinpeButton.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Image, Pressable, StyleSheet } from "react-native";
+
+const SinpeButton = ({ onPress }) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+    >
+      <Image
+        source={require("../../assets/sinpe-icon.png")}
+        style={styles.icon}
+      />
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 50,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#4C51F7",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    shadowOffset: { width: 0, height: 2 },
+    elevation: 5,
+  },
+  buttonPressed: {
+    opacity: 0.8,
+  },
+  icon: {
+    width: 25.27,
+    height: 21,
+    tintColor: "#4C51F7",
+  },
+});
+
+export default SinpeButton;
---
+++ navigation/MainNavigator.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreenScreen from "../screens/Home";
+
+const Stack = createStackNavigator();
+
+const MainNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreenScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainNavigator;
